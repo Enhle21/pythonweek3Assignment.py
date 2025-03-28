649a3b9892b8edcf4e2d017c5b19e487149dae7e
Enhle21 <tshabangupalesa272@gmail.com>
2025-03-28 10:27:56 +0200

    Add files via upload

---
+++ pythonweek3Assignment.py.py
@@ -0,0 +1,18 @@
+def calculate_discount(price, discount_percent):
+    """Calculate the final price price after applying a discount.
+    if the discount is 20% or higher, it applies the discount; otherwise, it returns the original price
+    """
+    if discount_percent >= 20:
+       final_price = price - (price * discount_percent / 100)
+       return final_price
+    else:
+        return price
+
+price = float (input("Enter the original price of the item:"))
+discount_percent = float(input("Enter the discount percentage :"))
+
+final_price = calculate_discount(price, discount_percent)
+if discount_percent >= 20:
+    print(f"The final price after applying the discount is :{final_price:.2f}")
+else:
+    print(f"No discount applied. The original price is :{price:.2f}")
